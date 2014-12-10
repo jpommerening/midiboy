1a5d6336ceb6760f9348ed881a1b3db3749ac5d2
Jonas Pommerening <jonas.pommerening@gmail.com>
2014-12-10 17:04:56 +0100

    Add some intitial midi message parsing

--- midi.c
+++ midi.c
@@ -28,3 +28,70 @@ ISR(USART0_RX_vect) {
 
 ISR(USART0_TX_vect) {
 }
+
+/**
+ * Determine how to handle the incoming message.
+ * @param byte the byte to look at.
+ * @return a positive integer, the total size of the
+ *         midi message, if the given byte marks the
+ *         start of a midi message.
+ * @return zero if the byte is a data-byte of another
+ *         midi-message
+ * @return -1 for reserved messages
+ * @return -2 for sysex messages
+ */
+int8_t midi_peek( const uint8_t byte ) {
+  static const int8_t msg_size_voice[8] = {
+    3, 3, 3, 3,
+    2, 2, 3, -1
+  };
+  static const int8_t msg_size_common[16] = {
+    -2, 2, 3, 2,
+    -1, -1, 1, 1,
+    1, -1, 1, 1,
+    1, -1, 1, 1
+  };
+  if (byte < 0x80) {
+    return 0;
+  } else if (byte < 0xf0) {
+    return msg_size_voice[(byte>>4) & 0x07];
+  } else {
+    return msg_size_common[byte & 0x0f];
+  }
+}
+
+/**
+ * Receive a single byte and store it in the internal buffer.
+ * @param in the midi-in port
+ * @param byte the byte that was received
+ */
+void midi_recv( midi_in_t* in, const uint8_t byte ) {
+  int8_t len = midi_peek( byte );
+  if (len == 0) {
+    if (in->sysx) {
+      /* handle sysex data */
+    } else {
+      in->msg.data[in->moff++] = byte;
+    }
+  } else if (len > 0) {
+    if (/* realtime? */ 0) {
+      /* handle realtime signals immediately */
+    } else {
+      in->msg.data[0] = byte;
+      in->mlen = len;
+      in->moff = 1;
+    }
+  } else if (len < 0) {
+    in->mlen = 0;
+    in->moff = 0;
+  }
+}
+
+void midi_poll( midi_in_t* in ) {
+  if (in->mlen == in->moff && in->mlen > 0) {
+    /* handle message */
+    in->recv( in, in->msg );
+    /* prepare for rolling status */
+    in->moff = 1;
+  }
+}
--- midi.h
+++ midi.h
@@ -1,9 +1,31 @@
-typedef struct {
-  unsigned rxen : 1;
-  unsigned txen : 1;
-  unsigned omni : 1;
-  unsigned pad_ : 1;
-  unsigned chan : 4;
-} midi_t;
+typedef struct midi_in_s midi_in_t;
 
-void midi_init( midi_t* port );
+typedef union {
+  uint32_t msg;
+  uint8_t  data[4];
+  struct {
+    unsigned type : 4;
+    unsigned chan : 4;
+    uint8_t  data[3];
+  } voice;
+  struct packed {
+    uint8_t type;
+    uint8_t data[3];
+  } common;
+} midi_msg_t;
+
+typedef void (*midi_recv_cb)( midi_in_t*, const midi_msg_t );
+typedef void (*midi_send_cb)( midi_in_t* );
+
+struct midi_in_s {
+  unsigned chan : 4;
+  unsigned omni : 1;
+  unsigned sysx : 1;
+  unsigned _pad : 2;
+
+  unsigned mlen : 4;
+  unsigned moff : 4;
+  midi_msg_t msg;
+
+  void (*recv)( midi_in_t* in, const midi_msg_t msg );
+};
